1cbaefbfbe3d16f3158448a62417f772a7be1e99
Elijah Mooring <vehmloewff@gmail.com>
2022-11-03 16:51:09 -0700

    chore: upgrade deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as pathUtils from 'https://deno.land/std@0.149.0/path/mod.ts'
-export * as utils from 'https://deno.land/x/dtils@1.2.3/mod.ts'
-export * as colors from 'https://deno.land/std@0.149.0/fmt/colors.ts'
+export * as pathUtils from 'https://deno.land/std@0.162.0/path/mod.ts'
+export * as utils from 'https://deno.land/x/dtils@1.6.1/mod.ts'
+export * as colors from 'https://deno.land/std@0.162.0/fmt/colors.ts'
